a7ae483536a775c4dffa0a8072fe13c68c6e457e
Dr. Suresh Ramasamy <sureshdr@gmail.com>
2018-03-25 22:14:21 +0800

    Add files via upload
    Initial work files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DelDup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDevice = new System.Windows.Forms.Label();
+            this.txtDevice = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tspgBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.tsDateTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblDevice
+            // 
+            this.lblDevice.AutoSize = true;
+            this.lblDevice.Location = new System.Drawing.Point(7, 11);
+            this.lblDevice.Name = "lblDevice";
+            this.lblDevice.Size = new System.Drawing.Size(55, 13);
+            this.lblDevice.TabIndex = 1;
+            this.lblDevice.Text = "Device ID";
+            // 
+            // txtDevice
+            // 
+            this.txtDevice.Font = new System.Drawing.Font("Source Code Pro Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDevice.ForeColor = System.Drawing.Color.Teal;
+            this.txtDevice.Location = new System.Drawing.Point(11, 34);
+            this.txtDevice.Name = "txtDevice";
+            this.txtDevice.ReadOnly = true;
+            this.txtDevice.Size = new System.Drawing.Size(267, 31);
+            this.txtDevice.TabIndex = 2;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(306, 35);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(92, 29);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check!";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsStatus,
+            this.tspgBar1,
+            this.tsDateTime});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 121);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(432, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // tsStatus
+            // 
+            this.tsStatus.Name = "tsStatus";
+            this.tsStatus.Size = new System.Drawing.Size(45, 17);
+            this.tsStatus.Text = "Status: ";
+            // 
+            // tspgBar1
+            // 
+            this.tspgBar1.Name = "tspgBar1";
+            this.tspgBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // tsDateTime
+            // 
+            this.tsDateTime.Name = "tsDateTime";
+            this.tsDateTime.Size = new System.Drawing.Size(109, 17);
+            this.tsDateTime.Text = "Random Date Time";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(14, 75);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(108, 30);
+            this.btnScan.TabIndex = 6;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 143);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtDevice);
+            this.Controls.Add(this.lblDevice);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "DelDup by Dr. Suresh Ramasamy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDevice;
+        private System.Windows.Forms.TextBox txtDevice;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tsStatus;
+        private System.Windows.Forms.ToolStripProgressBar tspgBar1;
+        private System.Windows.Forms.ToolStripStatusLabel tsDateTime;
+        private System.Windows.Forms.Button btnScan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Management;
+using System.Collections;
+using System.Data.SQLite;
+using System.IO;
+using sureshdr.libddupe;
+
+namespace DelDup
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArrayList hdArray = new ArrayList();
+            txtDevice.Text = "Processing...";
+            hdArray = GetDeviceID();
+            if (hdArray.Count > 0)
+            {
+                if (hdArray.Count == 1)
+                    txtDevice.Text = "[" + hdArray.Count + "] " + ((HardDrive)hdArray[0]).Model.Trim() + " [" + ((HardDrive)hdArray[0]).SerialNo.Trim()  + "]";
+                else
+                    txtDevice.Text = "[" + hdArray.Count + "] ";
+            }
+            else txtDevice.Text = "Error! No HDD found!";
+            tsDateTime.Text = DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString(); 
+        }
+
+        private void SaveDevice(ArrayList hdArr)
+        {
+
+
+        }
+
+        
+        private void DirEnum(string sFullPath, List<FileInfo> fileInfoList)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(sFullPath);
+                FileInfo[] files = di.GetFiles();
+
+                foreach (FileInfo file in files)
+                    fileInfoList.Add(file);
+
+                //Scan recursively
+                DirectoryInfo[] dirs = di.GetDirectories();
+                if (dirs == null || dirs.Length < 1)
+                    return;
+                MessageBox.Show(@"Contains #" + dirs.Length + "number of entries!");
+                foreach (DirectoryInfo dir in dirs)
+                {
+                    if (dir.FullName.StartsWith(@"C:\Windows") ||
+                        dir.FullName.StartsWith(@"C:\Program"))
+                    {
+                        MessageBox.Show("Contains " + dir.ToString());
+                    }
+                    DirEnum(dir.FullName, fileInfoList);
+                }
+            }
+            catch (Exception ex)
+            {
+               // Logger.Write("Exception in Helper.EnumerateFiles", ex);
+            }
+        }
+
+        private ArrayList GetDeviceID()
+        {
+            int counter = 0;
+            ArrayList hdCollection = new ArrayList();
+
+            using (ManagementObjectSearcher searcher = new
+            ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive"))
+            {
+
+                foreach (ManagementObject wmi_HD in searcher.Get())
+                {
+                    HardDrive hd = new HardDrive();
+                    hd.Model = wmi_HD["Model"].ToString();
+                    hd.Type = wmi_HD["InterfaceType"].ToString();
+                    hd.SerialNo = wmi_HD["SerialNumber"].ToString().Trim();
+                    hd.Guid = Guid.NewGuid().ToString();
+                    hdCollection.Add(hd);
+                    counter++;
+                }
+            }
+            return hdCollection;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            using (DataMgmt dm = new DataMgmt(Properties.Settings.Default.DBFile))
+            {
+                foreach (HardDrive hd in GetDeviceID())
+                {
+                    if (!dm.IsExistDrive(hd.SerialNo))
+                    {
+                        DialogResult rs = MessageBox.Show("Do you want to save " + hd.SerialNo + " ?", "Drive Save", MessageBoxButtons.YesNo);
+                        switch (rs)
+                        {
+                            case DialogResult.Yes:
+                                dm.AddDrive(hd);
+                                break;
+                            case DialogResult.No:
+
+                                break;
+                        }
+                    }
+                    else
+                        MessageBox.Show(hd.SerialNo + " already exists in the Master DB");
+                }
+                SaveDevice(GetDeviceID());
+            }
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            List<FileInfo> FileList = new List<FileInfo>();
+            DirEnum(@"C:\",  FileList);
+            MessageBox.Show("Final Count: " + FileList.Count);           
+        }
+    }
+
+}
